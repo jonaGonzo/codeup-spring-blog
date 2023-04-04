91d763cc55646757777d5ba46494e24a428a26ec
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-04-04 11:59:50 -0500

    partials added to sheets

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/AuthenticationController.java
@@ -0,0 +1,22 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthenticationController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String postLoginForm() {
+
+        return "redirect:/index";
+    }
+
+
+}
